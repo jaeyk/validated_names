9a3e1c12926977e298d77a2dcad1620d67e55d7f
Jae Yeon Kim <jaeyeonkim@berkeley.edu>
2021-08-18 14:49:22 -0700

    initial commit

---
+++ services/names-13.php
@@ -0,0 +1,15 @@
+<?php
+  const KEYS = ['name13'];
+
+  $file = 'names.csv';
+  $csv = file_get_contents($file);
+  $lines = explode("\n", $csv);
+  $num = count($lines);
+
+  $randomLineNumber = rand(0, $num - 1); // get random line number
+  $line = $lines[$randomLineNumber];     // get random line
+  $array = array_combine(KEYS, str_getcsv($line)); // map values to keys
+
+  header('content-type: application/json');
+  echo json_encode($array);
+?>
